25c993e8beede1e5465e72d8e768ebfd0744e4c0
Renato Perini <renato.perini@gmail.com>
2025-11-30 17:16:50 +0100

    Some renaming.

--- microk8s/rancher/certs/create_ca.sh
+++ microk8s/rancher/certs/create_ca.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+
+# Generate a Private Key for the generation of the Certificate Authority (CA)
 openssl genrsa -out localCA.key 2048
+# Now with this Private Key, generate the Certificate Authority (CA)
 openssl req -x509 -new -nodes -key localCA.key -sha256 -days 3650 -out localCA.crt \
-  -subj "/C=IT/ST=Lombardia/L=Milano/O=DigitalNucleus/OU=Dev/CN=Local Dev CA"
+  -subj "/C=IT/ST=Lombardia/L=Milano/O=VMStar International/OU=Dev/CN=VMStar Local Dev CA"
 cp localCA.crt /usr/local/share/ca-certificates
 # This will update the Trusted CA in /etc/ssl/certs
 update-ca-certificates
-# In windows, import the localCA.crt in certmgr.msc
-
+# In windows, import the localCA.crt in certmgr.msc in your Trusted Root Certification Authorities
--- microk8s/rancher/certs/generate_certs.sh
+++ microk8s/rancher/certs/generate_certs.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
 # Generate domain private key
-openssl genrsa -out digitalnucleus.it.key 2048
+openssl genrsa -out vmstar.lan.key 2048
 # Generate Certificate Signing Request (CSR) with the domain private key and the san.cnf configuation
-openssl req -new -key digitalnucleus.it.key -out digitalnucleus.it.csr -subj "/CN=*.digitalnucleus.it" -config san.cnf
+openssl req -new -key vmstar.lan.key -out vmstar.lan.csr -subj "/CN=*.vmstar.lan" -config san.cnf
 # Generate certificate and private key signing with the CA
-openssl x509 -req -in digitalnucleus.it.csr -CA localCA.crt -CAkey localCA.key \
-  -CAcreateserial -out digitalnucleus.it.crt -days 825 -sha256 \
+openssl x509 -req -in vmstar.lan.csr -CA localCA.crt -CAkey localCA.key \
+  -CAcreateserial -out vmstar.lan.crt -days 825 -sha256 \
   -extensions v3_req -extfile san.cnf
-
+# Generate the full certificate (CA + Cert)
+cat vmstar.lan.crt > full_cert.crt
+cat localCA.crt >> full_cert.crt
+# Rename them with much more useful names
+mv vmstar.lan.key tls.key
+mv vmstar.lan.crt tls.crt
