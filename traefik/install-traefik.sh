552e5ecd8b0b86bd75a37087e1bdbed4ae26b8ac
Renato Perini <renato.perini@gmail.com>
2025-12-03 23:50:11 +0100

    Mainly restructuring the main test apps.

--- traefik/install-traefik.sh
+++ traefik/install-traefik.sh
@@ -4,6 +4,8 @@ helm repo add traefik https://traefik.github.io/charts
 helm repo update
 kubectl create ns traefik-gateway
 kubectl create -f ./externalsecret.yaml
+echo "Waiting 4 seconds for certificate propagation ..."
+sleep 4
 kubectl create -f ./nginx-ingressclass.yaml
 kubectl create -f ./crd/1.4.0/standard-install.yaml
 helm upgrade --install traefik-gateway traefik/traefik \
