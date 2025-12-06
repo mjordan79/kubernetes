23ce47b25e8f199da730429b364c3f0e2cda5f21
Renato Perini <renato.perini@gmail.com>
2025-12-06 01:12:04 +0100

    Adding the possibility to not choose official Gateway API CRDs during install

--- traefik/install-traefik.sh
+++ traefik/install-traefik.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+WITH_SIGS_API=$1
+
 helm repo add traefik https://traefik.github.io/charts
 helm repo update
 kubectl create ns traefik-gateway
@@ -7,6 +9,9 @@ kubectl create -f ./externalsecret.yaml
 echo "Waiting 4 seconds for certificate propagation ..."
 sleep 4
 kubectl create -f ./nginx-ingressclass.yaml
-kubectl create -f ./crd/1.4.0/standard-install.yaml
+if [[ "$WITH_SIGS_API" == "--with-sigs-api" ]];
+then
+  kubectl create -f ./crd/1.4.1/standard-install.yaml
+fi
 helm upgrade --install traefik-gateway traefik/traefik \
-  -f ./values.yaml -n traefik-gateway --version 37.4.0 --create-namespace
+  -f ./values.yaml -n traefik-gateway --version 37.4.0 --set image.tag=3.6.4 --create-namespace
