6b708e6005cfd98fe06bde812b0025a82e019b54
Renato Perini <renato.perini@gmail.com>
2025-12-07 05:09:09 +0100

    Semi-final configuration for making Traefik working without any conflit near to the nginx-ingress controller.

--- traefik/remove-traefik.sh
+++ traefik/remove-traefik.sh
@@ -3,7 +3,6 @@
 helm uninstall traefik-gateway -n traefik-gateway
 helm repo remove traefik
 helm repo update
-kubectl delete -f ./nginx-ingressclass.yaml
 kubectl delete -f ./crd/1.4.0/standard-install.yaml
 kubectl delete -f ./externalsecret.yaml
 kubectl delete ns traefik-gateway
